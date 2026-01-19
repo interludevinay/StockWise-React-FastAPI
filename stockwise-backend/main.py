6802e2c341ead41a55051976f44eb51e3444a55c
interludevinay <vinay1270sharma@gmail.com>
2026-01-19 18:18:57 +0530

    fix: Modified routes

--- stockwise-backend/main.py
+++ stockwise-backend/main.py
@@ -10,7 +10,9 @@ database_models.Base.metadata.create_all(bind=engine)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+    ],
     allow_methods=["*"],
     allow_credentials=True,
     allow_headers=["*"],
